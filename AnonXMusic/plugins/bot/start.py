10112a74d940a3544eec784f688cc38342ed9c76
RiyoGod <godsxrio@gmail.com>
2025-03-21 14:04:45 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -25,8 +25,8 @@ from strings import get_string
 
 # 📌 Random Start Images
 START_IMAGES = [
-    "https://files.catbox.moe/hr2v68.png",
-    "https://files.catbox.moe/ojtwi6.png",
+    "https://files.catbox.moe/w537cm.png",
+    "https://files.catbox.moe/4eitcx.png",
 ]
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
